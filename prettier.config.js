972b394c86e1b512f08bad2461a91803af296e67
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2025-08-20 19:57:33 +0530

    feat: initial commit by @harshv5094

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/**@type {import("prettier").Config}*/
+
+const config = {
+  trailingComma: 'none',
+  tabWidth: 2,
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: "avoid",
+  endOfLine: 'lf',
+  semi: false,
+}
+
+export default config
