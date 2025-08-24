38cbbd280b957333b717ba553ca3c203b70c8323
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2025-08-24 23:21:23 +0530

    feat(css): setting up new fonts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,6 +8,9 @@ function Home() {
       <button onClick={toggleTheme}>
         <p>Change to: {theme}</p>
       </button>
+      <p className="font-sans">IBM Plex Sans text</p>
+      <p className="font-serif">IBM Plex Serif text</p>
+      <p className="font-mono">JetBrains Mono text</p>
     </div>
   )
 }
