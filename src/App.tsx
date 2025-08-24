3b00ad7377aa4ff2711522f79d302052ce9792c1
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2025-08-24 19:30:55 +0530

    feat: adding base toggle theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Work from "./pages/Work"
+import { ThemeProvider } from "./context/themeToggle"
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/aboutme" element={<About />} />
-        <Route path="/work" element={<Work />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/aboutme" element={<About />} />
+          <Route path="/work" element={<Work />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,13 @@
+import { useTheme } from "../context/themeToggle"
+
 function Home() {
+  const { theme, toggleTheme } = useTheme()
   return (
     <div className="text-2xl">
       <h1>Home Page</h1>
+      <button onClick={toggleTheme}>
+        <p>Change to: {theme}</p>
+      </button>
     </div>
   )
 }
