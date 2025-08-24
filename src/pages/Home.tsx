27ed7481743b02f9a9dc69a4f4947b6eb89df837
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2025-08-24 17:11:37 +0530

    feat: Skeletal pages

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="text-2xl">
+      <h1>Home Page</h1>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/Work.tsx
@@ -0,0 +1,9 @@
+function Work() {
+  return (
+    <div className="text-2xl">
+      <h1>Work Page</h1>
+    </div>
+  )
+}
+
+export default Work
